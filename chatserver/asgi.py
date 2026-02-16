ecd8257a1f077b529eeddd433c5f7da89360e277
shen <shenleekhalid@gmail.com>
2026-02-16 19:21:02 +0300

    installing channels and daphne

--- chatserver/asgi.py
+++ chatserver/asgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+from channels.routing import ProtocolTypeRouter
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatserver.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http' : django_asgi_app
+})
