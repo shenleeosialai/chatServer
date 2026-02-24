64e92c46a50e7670161c96c2d92912b1040d69a0
shen <shenleekhalid@gmail.com>
2026-02-24 18:13:20 +0300

    modifying the connect method

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,17 @@
 import json
+from asgiref.sync import async_to_async
 from channels.generic.websocket import WebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(WebsocketConsumer):    
     def connect(self):
+        self.id = self.scope['url_route']['kwargs']['user_id']
+        self.room_group_name = f'chat_{self.id}'
+        # Join room group
+        async_to_async(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         self.accept()
     
     def disconnect(self, close_code):
