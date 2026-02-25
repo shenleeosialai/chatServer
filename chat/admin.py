88de01ec92b9238d0d28df44727b3e8c59aa5356
shen <shenleekhalid@gmail.com>
2026-02-25 12:42:04 +0300

    fixed admin error,(admin.E003) The value of 'raw_id_fields[1]' must be a foreign key or a many-to-many field.

--- chat/admin.py
+++ chat/admin.py
@@ -8,4 +8,4 @@ class MessageAdmin(admin.ModelAdmin):
     list_display = ['sent_on', 'user', 'content']
     list_filter = ['sent_on']
     search_fields = ['content']
-    raw_id_fields = ['user', 'content']
+    raw_id_fields = ['user']
