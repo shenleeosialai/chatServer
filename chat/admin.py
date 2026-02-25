667e115b9c3df03dc627734d31ab937ac703ff2c
shen <shenleekhalid@gmail.com>
2026-02-25 12:34:00 +0300

    models and admin for chat messages

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from chat.models import Message
 
-# Register your models here.
+
+@admin.register(Message)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['sent_on', 'user', 'content']
+    list_filter = ['sent_on']
+    search_fields = ['content']
+    raw_id_fields = ['user', 'content']
